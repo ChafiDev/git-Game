f17cb78c613142718831fda84785a114f6d78094
ChafiDev <juan.k.ita@hotmail.es>
2019-02-04 01:00:19 -0500

    adding the main function to play

--- game.js
+++ game.js
@@ -40,3 +40,53 @@ function humanchoos(t){
   }while(h != 1);
   return t
 }
+
+function letsPlay(funcion1, funcion2){
+  var round = 0
+  var cpuWin = 0
+  var humanWin = 0
+  var tie = 0
+  var hres = 0
+  var cres = 0
+  var i = 1
+
+  while(round < 3){
+    round++
+    var c = cpuChoise(c)
+    var o = humanchoos(o)
+
+    if ((c == 'Rock' && o == 'Scissors') || (c == 'Paper' && o == 'Rock') || (c == 'Scissors' && o == 'Paper')) {
+      alert(`Round: ${round} \nCPU's choise: ${c} beat Your choise: ${o} \nYou lose!!!`);
+      cpuWin++
+    }else if ((o == 'Rock' && c == 'Scissors') || (o == 'Paper' && c == 'Rock') || (o == 'Scissors' && c == 'Paper')) {
+      alert(`Round: ${round} \nYour choise: ${o} beat CPU's choise: ${c} \nYou win!!!`);
+      humanWin++
+    }else {
+      alert(`Round: ${round} \n Tie!!!!`)
+      tie++
+    }
+  }
+
+  hres = humanWin
+  cres = cpuWin
+
+  var humanRes = document.getElementById("humanRes");
+  humanRes.innerHTML = `You win ${humanWin} times`;
+
+  var cpuRes = document.getElementById("cpuRes");
+  cpuRes.innerHTML = `CPU win ${cpuWin} times`;
+
+  var ties = document.getElementById("tie");
+  ties.innerHTML = `TIE: ${tie}`;
+
+  if ((hres > cres)) {
+    var total = document.getElementById("total")
+    total.innerHTML = `You are the CHAMPION!!!`
+  }else if((hres < cres)) {
+    var total = document.getElementById("total")
+    total.innerHTML = `Sorry CPU is the CHAMPION!!!`
+  }else{
+    var total = document.getElementById("total")
+    total.innerHTML = `It's a TIE!!! Try again.`
+  }
+}
