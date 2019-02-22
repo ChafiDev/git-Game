d209a73c6a9844a33f8030436ee6b1c267f1c323
ChafiDev <juan.k.ita@hotmail.es>
2019-02-21 19:37:41 -0500

    Addin semicolons

--- game.js
+++ game.js
@@ -1,73 +1,73 @@
 function cpuChoise(choise){
-  var cpuC = ''
-  var cpuN = Math.random()
+  var cpuC = '';
+  var cpuN = Math.random();
 
   if((cpuN > 0 && cpuN < 0.34)){
-    cpuC = 'Rock'
+    cpuC = 'Rock';
   }else if (cpuN > 0.34 && cpuN < 0.66) {
-    cpuC = 'Paper'
+    cpuC = 'Paper';
   }else {
-    cpuC = 'Scissors'
+    cpuC = 'Scissors';
   }
-  return cpuC
+  return cpuC;
 }
 
 function humanchoos(t){
-  var h = 0
+  var h = 0;
   do{
-    var t = window.prompt('Tipe Rock, Paper or Scissors').toLowerCase()
+    var t = window.prompt('Tipe Rock, Paper or Scissors').toLowerCase();
 
     switch (t) {
       case 'rock':{
-        t = 'Rock'
+        t = 'Rock';
         h = 1
       }break;
       case 'paper':{
-        t = 'Paper'
+        t = 'Paper';
         h = 1
       }break;
       case 'scissors':{
-        t = 'Scissors'
-        h = 1
+        t = 'Scissors';
+        h = 1;
       }break;
       case '':{
-        h = 0
+        h = 0;
       }break;
       default :{
-        h = 0
+        h = 0;
       }break;
     }
   }while(h != 1);
-  return t
+  return t;
 }
 
 function letsPlay(funcion1, funcion2){
-  var round = 0
-  var cpuWin = 0
-  var humanWin = 0
-  var tie = 0
-  var hres = 0
-  var cres = 0
+  var round = 0;
+  var cpuWin = 0;
+  var humanWin = 0;
+  var tie = 0;
+  var hres = 0;
+  var cres = 0;
 
   while(round < 5){
     round++
-    var c = cpuChoise(c)
-    var o = humanchoos(o)
+    var c = cpuChoise(c);
+    var o = humanchoos(o);
 
     if ((c == 'Rock' && o == 'Scissors') || (c == 'Paper' && o == 'Rock') || (c == 'Scissors' && o == 'Paper')) {
       alert(`Round: ${round} \nCPU's choise: ${c} beat Your choise: ${o} \nYou lose!!!`);
-      cpuWin++
+      cpuWin++;
     }else if ((o == 'Rock' && c == 'Scissors') || (o == 'Paper' && c == 'Rock') || (o == 'Scissors' && c == 'Paper')) {
       alert(`Round: ${round} \nYour choise: ${o} beat CPU's choise: ${c} \nYou win!!!`);
-      humanWin++
+      humanWin++;
     }else {
-      alert(`Round: ${round} \n Tie!!!!`)
-      tie++
+      alert(`Round: ${round} \n Tie!!!!`);
+      tie++;
     }
   }
 
-  hres = humanWin
-  cres = cpuWin
+  hres = humanWin;
+  cres = cpuWin;
 
   var humanRes = document.getElementById("humanRes");
   humanRes.innerHTML = `You win ${humanWin} times`;
@@ -91,9 +91,9 @@ function letsPlay(funcion1, funcion2){
 }
 
 function go(){
-  var cpuC2, human2
+  var cpuC2, human2;
   letsPlay(cpuChoise(cpuC2), humanchoos(human2))
 }
 
-var but = document.getElementById('play')
+var but = document.getElementById('play');
 but.addEventListener('click', go);
